dd4469dd6ff2e0dbff90b5202e7ef25a050a4225
Ben McKerry <110857332+bmckerry@users.noreply.github.com>
2024-08-13 12:10:51 -0400

    fix tests

--- py/test_parse_and_notify.py
+++ py/test_parse_and_notify.py
@@ -2,95 +2,60 @@ from typing import Sequence
 
 import pytest
 
-from parse_and_notify import (
-    NewTopic,
-    UpdatedTopic,
-    make_markdown_table,
-    make_slack_message,
-)
+from parse_and_notify import Destinations, NewTopic, UpdatedTopic, make_table
 
-DD_TABLE_TESTS = [
-    pytest.param([], [], "%%%\n||\n||\n%%%", id="Empty table"),
-    pytest.param(
-        ["col1", "col2"],
-        [],
-        "%%%\n|col1|col2|\n|-|-|\n%%%",
-        id="Table with header",
-    ),
-    pytest.param(
-        ["col1", "col2"],
-        [["val1", "val2"], ["val3", "val4"]],
-        "%%%\n|col1|col2|\n|-|-|\n|val1|val2|\n|val3|val4|\n%%%",
-        id="Table with header and rows",
-    ),
-]
-
-SLACK_TABLE_TESTS = [
+TABLE_TESTS = [
     pytest.param([], [], "", id="Empty table"),
-    pytest.param(
-        ["col1", "col2"],
-        [],
-        "```| col1 | col2 |\n| ----------- |\n```",
-        id="Table with header",
-    ),
+    pytest.param(["col1", "col2"], [], "", id="Table with just headers"),
     pytest.param(
         ["col1", "col2"],
         [["val1", "val2"], ["val3", "val4"]],
-        "```| col1 | col2 |\n| ----------- |\n| val1 | val2 |\n| val3 | val4 |\n```",  # noqa
+        "```| col1 | col2 |\n| ---- | ---- |\n| val1 | val2 |\n| val3 | val4 |\n```",  # noqa
         id="Table with header and rows",
     ),
 ]
 
 
-@pytest.mark.parametrize("headers, content, expected", DD_TABLE_TESTS)
-def test_markdown_table(
+@pytest.mark.parametrize("headers, content, expected", TABLE_TESTS)
+def test_table(
     headers: Sequence[str],
     content: Sequence[Sequence[str | int | None]],
     expected: str,
 ) -> None:
-    assert make_markdown_table(headers, content, False, None) == expected
-
-
-@pytest.mark.parametrize("headers, content, expected", SLACK_TABLE_TESTS)
-def test_slack_table(
-    headers: Sequence[str],
-    content: Sequence[Sequence[str | int | None]],
-    expected: str,
-) -> None:
-    assert make_slack_message(headers, content, False, None) == expected
+    assert make_table(headers, content, None, Destinations.SLACK) == expected
 
 
 NEW_TOPIC_RENDERED = """%%%
-|Parameter|Value|
-|-|-|
-|Topic Name|my_topic|
-|Action (create/update)|create|
-|Partition Count|16|
-|Replication Factor|3|
-|cleanup.policy|delete|
-|max.message.bytes|5542880|
+| Parameter              | Value    |
+| ---------------------- | -------- |
+| Topic Name             | my_topic |
+| Action (create/update) | create   |
+| Partition Count        | 16       |
+| Replication Factor     | 3        |
+| cleanup.policy         | delete   |
+| max.message.bytes      | 5542880  |
 %%%"""
 
 EMPTY_ERROR_MESSAGE = """%%%
 # ERROR - the following error occurred while processing this topic:
 this is an error
 
-# No changes were made.%%%"""
+# No changes were made.
+%%%"""
 
 UPDATE_CHANGES_ERROR_MESSAGE = """%%%
 # ERROR - the following error occurred while processing this topic:
 also an error
 
 # The following changes were still made:
-
-|Parameter|Old Value|New Value|
-|-|-|-|
-|Action (create/update)|update||
-|cleanup.policy||delete|
-|message.timestamp.type|CreateTime|LogAppendTime|
-|max.message.bytes||REMOVED|
-|Partition 0 assignments|[5, 4]|[3, 4]|
-|Partition 1 assignments|[2, 6]|[5, 6]|
+| Parameter               | Old Value  | New Value     |
+| ----------------------- | ---------- | ------------- |
+| Action (create/update)  | update     |               |
+| cleanup.policy          |            | delete        |
+| message.timestamp.type  | CreateTime | LogAppendTime |
+| max.message.bytes       |            | REMOVED       |
+| Partition 0 assignments | [5, 4]     | [3, 4]        |
+| Partition 1 assignments | [2, 6]     | [5, 6]        |
 %%%"""
 
 
@@ -104,8 +69,7 @@ def test_topicctl() -> None:
             {"name": "cleanup.policy", "value": "delete"},
             {"name": "max.message.bytes", "value": "5542880"},
         ],
-        "error": False,
-        "errorMessage": "",
+        "errorMessage": None,
         "dryRun": False,
     }
     updated_topic_content = {
@@ -133,8 +97,7 @@ def test_topicctl() -> None:
                 "updatedReplicas": [5, 6],
             },
         ],
-        "error": False,
-        "errorMessage": "",
+        "errorMessage": None,
         "dryRun": False,
     }
 
@@ -148,7 +111,7 @@ def test_topicctl() -> None:
         ["cleanup.policy", "delete"],
         ["max.message.bytes", "5542880"],
     ]
-    assert topic.render_table() == NEW_TOPIC_RENDERED
+    assert topic.render_table(Destinations.DATADOG) == NEW_TOPIC_RENDERED
 
     assert isinstance(topic2, UpdatedTopic)
 
@@ -169,7 +132,6 @@ def test_topicctl_errors() -> None:
         "numPartitions": None,
         "replicationFactor": None,
         "configEntries": [],
-        "error": True,
         "errorMessage": "this is an error",
         "dryRun": False,
     }
@@ -198,7 +160,6 @@ def test_topicctl_errors() -> None:
                 "updatedReplicas": [5, 6],
             },
         ],
-        "error": True,
         "errorMessage": "also an error",
         "dryRun": False,
     }
@@ -209,7 +170,7 @@ def test_topicctl_errors() -> None:
     # change_set is empty because no changes are in the dict
     assert topic.change_set == []
 
-    assert topic.render_table() == EMPTY_ERROR_MESSAGE
+    assert topic.render_table(Destinations.DATADOG) == EMPTY_ERROR_MESSAGE
 
     assert isinstance(topic2, UpdatedTopic)
 
@@ -222,4 +183,7 @@ def test_topicctl_errors() -> None:
         ["Partition 1 assignments", "[2, 6]", "[5, 6]"],
     ]
 
-    assert topic2.render_table() == UPDATE_CHANGES_ERROR_MESSAGE
+    assert (
+        topic2.render_table(Destinations.DATADOG)
+        == UPDATE_CHANGES_ERROR_MESSAGE
+    )
